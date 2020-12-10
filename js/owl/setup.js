d41ce450dfd24648c366673f795dfbd40e053f49
Diogonsc <diogon21@yahoo.com.br>
2020-12-10 19:40:47 -0200

    Primeiro Commit

---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+// lista de filmes
+
+$('.owl-carousel').owlCarousel({
+    video:true,
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:3.5
+        },
+        600:{
+            items:3.5
+        },
+        1000:{
+            items:5.5
+        }
+    }
+})
+
+
+// lista previas
+
+
+
+
